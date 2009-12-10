df410a8d5019255e3b97d0c50c40881a6df5d0b3
joe bartlett <contact@jdbartlett.com>
2009-12-10 17:23:58 -0600

    demo & fix to help stop stuck magnifier
    * occasionally, magnifier gets stuck after frantic mouse movement. fixes that.
    * added demo page for user to d/l

--- jquery.loupe.js
+++ jquery.loupe.js
@@ -5,46 +5,34 @@
 (function($) {
 	$.fn.loupe = function(options) {
 		if (!this.length) return this;
-		options = $.extend({
-			loupe: 'loupe',
-			width: 200,
-			height: 150
-		}, options || {});
+		options = $.extend({loupe:'loupe', width:200, height:150}, options || {});
 
 		this.each(function() {
-			var $this = $(this), loupe = null, big = null, small = null,
+			var $this = $(this), loupe = null, big = null, small = null, time = null,
+			hide = function() {loupe.hide()},
 			move = function(e) {
-				var offset = small.offset(), smallWidth = small.width(), smallHeight = small.height();
-				if (
-					e.pageX > smallWidth + offset.left + 10 || e.pageX < offset.left - 10 ||
-					e.pageY > smallHeight + offset.top + 10 || e.pageY < offset.top - 10
-				) return loupe.hide();
-				loupe.css({
-					left:e.pageX - options.width/2,
-					top:e.pageY - options.height/2
-				})
+				var os = small.offset(), sW = small.width(), sH = small.height(), oW = options.width/2, oH = options.height/2;
+				if (e.pageX > sW + os.left + 10 || e.pageX < os.left - 10 ||
+					e.pageY > sH + os.top + 10 || e.pageY < os.top - 10) return hide();
+				if (time && clearTimeout(time)) time = null;
+				loupe.css({left:e.pageX - oW, top:e.pageY - oH});
 				big.css({
-					left: -(((e.pageX - offset.left) / smallWidth) * big.width() - options.width/2)|0,
-					top: -(((e.pageY - offset.top) / smallHeight) * big.height() - options.height/2)|0
+					left:-(((e.pageX - os.left) / sW) * big.width() - oW)|0,
+					top:-(((e.pageY - os.top) / sH) * big.height() - oH)|0
 				});
-			}
+			};
 
 			$this.mouseenter(function(e) {
 				if (!small) small = $this.is('img') ? $this : $this.find('img:first');
-				loupe = (loupe || (loupe = $('<div></div>')
-					.addClass(options.loupe)
+				loupe = (loupe || (loupe = $('<div></div>').addClass(options.loupe)
 					.css({
-						width:options.width+'px', height:options.height+'px',
+						width:options.width, height:options.height,
 						position:'absolute', overflow:'hidden'
 					})
 					.append(big = $('<img src="' + $this.attr($this.is('img') ? 'src' : 'href') + '" />').css({position:'absolute'}))
-					.mousemove(move)
-					.appendTo('body')
-				)).show();
-				move(e);
-			}).click(function() {
-				return false;
-			});
+					.mousemove(move).appendTo('body')
+				)).show(); move(e);
+			}).mouseout(function() {time = setTimeout(hide, 10)});
 		});
 
 		return this;
