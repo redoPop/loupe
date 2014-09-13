074e0e5cbbacea4d4cf785a7b63d1b02ab55d60b
Joe Bartlett <contact@jdbartlett.com>
2014-09-13 17:47:03 -0500

    Prevent touch screens from triggering the loupe

--- jquery.loupe.js
+++ jquery.loupe.js
@@ -14,21 +14,47 @@
 		return this.length ? this.each(function () {
 			var $this = $(this), $big, $loupe,
 				$small = $this.is('img') ? $this : $this.find('img:first'),
-				move, hide = function () { $loupe.hide(); },
+				isTouched,
+				isUntouched,
 				time;
 
 			if ($this.data('loupe') != null) {
 				return $this.data('loupe', arg);
 			}
 
-			move = function (e) {
+			function onTouchStart() {
+				isTouched = true;
+			}
+
+			function onPointerDown(event) {
+				if (event.pointerType === 'touch') {
+					onTouchStart();
+				}
+			}
+
+			function hide() {
+				isTouched = false;
+				isUntouched = false;
+				$loupe.hide();
+			}
+
+			function move(e) {
 				var os = $small.offset(),
 					sW = $small.outerWidth(),
 					sH = $small.outerHeight(),
 					oW = options.width / 2,
 					oH = options.height / 2;
 
-				if (!$this.data('loupe') ||
+				// Ignore the first movement event -- it could be
+				// a simulated mouse event! (Thanks, Android!)
+				if (!isUntouched) {
+					isUntouched = true;
+					return;
+				}
+
+				if (
+					isTouched ||
+					!$this.data('loupe') ||
 					e.pageX > sW + os.left + 10 || e.pageX < os.left - 10 ||
 					e.pageY > sH + os.top + 10 || e.pageY < os.top - 10) {
 					return hide();
@@ -44,7 +70,7 @@
 					left: -(((e.pageX - os.left) / sW) * $big.width() - oW)|0,
 					top: -(((e.pageY - os.top) / sH) * $big.height() - oH)|0
 				});
-			};
+			}
 
 			$loupe = $('<div />')
 				.addClass(options.loupe)
@@ -60,8 +86,16 @@
 				.hide()
 				.appendTo('body');
 
-			$this.data('loupe', true)
-				.mouseenter(move)
+			$this
+				// Prevent touch screens from triggering the loupe
+				.on('touchstart', onTouchStart)
+
+				// https://gist.github.com/redoPop/9050999cebcd7e50934a
+				.on('pointerdown', onPointerDown)
+				.on('MSPointerDown', onPointerDown)
+
+				.data('loupe', true)
+				.mousemove(move)
 				.mouseout(function () {
 					time = setTimeout(hide, 10);
 				});
