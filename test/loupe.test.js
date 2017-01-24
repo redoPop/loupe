d4c3a6170ecc4adbfdcc7c6c9e48af03e04b0626
Joe Bartlett <joe@redopop.com>
2017-01-23 23:00:24 -0600

    Use a simple Mocha + JSDom testing environment

--- test/loupe.test.js
+++ test/loupe.test.js
@@ -1,5 +1,54 @@
-QUnit.test('Loupe instance', function (assert) {
-  var loupe = new Loupe(new Image());
+var assert = require('assert');
+var jsdom = require('mocha-jsdom');
 
-  assert.equal(typeof loupe, 'object', 'should be an object');
+jsdom();
+
+describe('Loupe', function () {
+  var Loupe;
+
+  before(function () {
+    Loupe = require('../loupe');
+  });
+
+  describe('instance', function () {
+    var loupe;
+
+    beforeEach(function () {
+      loupe = new Loupe(document.createElement('img'));
+    })
+
+    it('should be an object', function () {
+      assert.equal(typeof loupe, 'object');
+    });
+
+    it('should have a lens with the "lens" class', function () {
+      assert.ok(loupe.lens.className.includes(loupe.classes.lens));
+    });
+
+    describe('show()', function () {
+      it('should append the "on" class to the lens', function () {
+        loupe.show();
+        assert.ok(loupe.lens.className.includes(loupe.classes.lens + ' ' + loupe.classes.on));
+      });
+
+      it('should remove the "off" class from the lens', function () {
+        loupe.hide();
+        loupe.show();
+        assert.ok(!loupe.lens.className.includes(loupe.classes.off));
+      });
+    });
+
+    describe('hide()', function () {
+      it('should append the "off" class to the lens', function () {
+        loupe.hide();
+        assert.ok(loupe.lens.className.includes(loupe.classes.lens + ' ' + loupe.classes.off));
+      });
+
+      it('should remove the "on" class from the lens', function () {
+        loupe.show();
+        loupe.hide();
+        assert.ok(!loupe.lens.className.includes(loupe.classes.on));
+      });
+    });
+  });
 });
